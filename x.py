f048ed9e52ce15f8a594715e39f0a823060ed38c
David Lutton <david@dalun.space>
2016-11-28 22:40:16 +0000

    x.py sample for Python syntax highlighting

---
+++ x.py
@@ -0,0 +1,8 @@
+import numpy as np  # Comment1
+
+class MyClass(Yourclass):
+    def __init__(self, my, yours):
+        String = "String"
+        String2 = '5 1 2 3 4'
+        print(String2)
+        # Comment2
