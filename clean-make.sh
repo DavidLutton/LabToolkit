e88cec61d1b8846ecba1f259daed877007e4bd8a
David Lutton <david@dalun.space>
2016-11-28 22:34:27 +0000

    Clean: leave .py files intact

--- clean-make.sh
+++ clean-make.sh
@@ -1,1 +1,6 @@
-find . -maxdepth 1 -type f ! -name "*.tex" ! -name "*.sh"  -delete && latexmk -pdf   RF\ Immunity\ Test\ System.tex  
+
+#!/bin/bash
+set -x
+
+#find . -maxdepth 1 -type f ! -name "*.tex" ! -name "*.sh"  ! -name "*.py"  -delete && latexmk -pdf -pvc RF\ Immunity\ Test\ System.tex
+find . -maxdepth 1 -type f ! -name "*.tex" ! -name "*.sh"  ! -name "*.py"  -delete && latexmk -pdf RF\ Immunity\ Test\ System.tex && latexmk -c
