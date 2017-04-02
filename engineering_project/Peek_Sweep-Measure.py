c2c8d3f4888c889a1224264af77dcf660e4db3af
David Lutton <david@dalun.space>
2017-04-02 21:52:38 +0100

    This should work for calibration with minor tweaking

--- engineering_project/Peek_Sweep-Measure.py
+++ engineering_project/Peek_Sweep-Measure.py
@@ -15,10 +15,10 @@ from datetime import datetime
 
 
 # Imports from modules installed by pip
-import visa  # [bindings to the "Virtual Instrument Software Architecture" in order to control measurement devices and test equipment via GPIB, RS232, or USB.](https://github.com/hgrecco/pyvisa/)
+import visa  # [bindings to VISA to control test equipment via GPIB, RS232, IP or USB](https://github.com/hgrecco/pyvisa/)
 import numpy as np  # [NumPy](http://www.numpy.org/)
 import pandas as pd  # [pandas: Python Data Analysis Library](http://pandas.pydata.org/)
-from openpyxl import Workbook  # [openpyxl - A Python library to read/write Excel 2010 xlsx/xlsm files — openpyxl documentation](https://openpyxl.readthedocs.io/en/default/)
+from openpyxl import Workbook  # [Python library to read/write Excel 2010 xlsx/xlsm files](https://openpyxl.readthedocs.io/en/default/)
 
 
 # --- Import of code of RCI
@@ -40,49 +40,27 @@ import Instrument.SignalGenerator
 import Instrument.SpectrumAnalyser
 
 # import Instrument.FunctionGenerator
-'''
-HP 33120A 15MHz
-Keysight 33500B 20MHz
-HP 8116A 50MHz
-'''
-# import Instrument.SignalGenerator
-'''
-HP 8673M 2-18GHz
-Anritsu MG3710A 100e3, 6e9
-Agilent N5182A 100e3, 6e9
-Marconi 2031 10e3-2.7e9
-Marconi 20nn 10e3-5.4e9
-'''
-# import Instrument.PowerMeter
-'''
-Bird 4421
-'''
+# HP 33120A 15MHz
+# Keysight 33500B 20MHz
+# HP 8116A 50MHz
+
 # import Instrument.FieldStrength
-'''
-EMC-20
-SI-100
-EMCO 7110
-'''
+# EMC-20 # setup & readback DONE
+# SI-100
+# EMCO 7110
+
 # import Instrument.Positioner
-'''
-2090 H
-'''
+# 2090 H
+
 # import Instrument.DMM
-'''
-HP 34401
-HP 3478A
-Longscale
-'''
+# HP 34401  # readback
+# HP 3478A # readback
+# Longscale
+
 # import Instrument.NetworkAnalyser
-'''
-E8357A
-4395A
-'''
-# import Instrument.SpectrumAnalyser
-'''
-8594E 9e3-40e9
-8653E -26.5e9
-'''
+# E8357A
+# 4395A
+
 # import Instrument.Osciliscope  # TDS 544A 500e5, DSO5052A 500e6 4GSa/s
 # import Instrument.SourceDC  # 6632A 0-20V 0-5A 100W
 # import Instrument.SourceAC  # 3001i, 3001iM
@@ -99,9 +77,7 @@ formatter = logging.Formatter(formatstr)
 #  %(name)s
 
 # re.sub(r'[]/\;,><&*:%=+@!#^()|?^', '', logger)
-logger = re.sub(r':', '', datetime.now().isoformat() + ".log")
-# print(logger)
-# python3.6 isoformat(timespec='seconds')
+logger = re.sub(r':', '', datetime.now().isoformat() + ".log")  # use regex fix illegal filename
 
 fh = logging.FileHandler(logger)
 fh.setFormatter(formatter)
@@ -110,7 +86,6 @@ fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 ch.setLevel(logging.DEBUG)
-
 
 log.addHandler(fh)
 log.addHandler(ch)
@@ -153,17 +128,29 @@ with ResourceManager('') as rm:
         "ANRITSU,MG3693A,": Instrument.SignalGenerator.AnritsuMG3693A,
         "Agilent Technologies, E4422B,": Instrument.SignalGenerator.AgilentE4422B,
         # Willtron 10e6, 40e9
+        '''
+        HP 8673M 2-18GHz
+        Anritsu MG3710A 100e3, 6e9
+        Agilent N5182A 100e3, 6e9
+        Marconi 2031 10e3-2.7e9
+        Marconi 20nn 10e3-5.4e9
+        '''
     })
 
     PowerMeter = driverdispatcher(pool, {
         "HEWLETT-PACKARD,437B,": Instrument.PowerMeter.HP437B,
         "Agilent Technologies,E4418B,": Instrument.PowerMeter.AgilentE4418B,
         # NVRS
+        # R&S ???4
+        # Bird 4421
     })
 
     SpectrumAnalyser = driverdispatcher(pool, {
         "Hewlett-Packard,E4406A,": Instrument.SpectrumAnalyser.HPE4406A,
         "Agilent Technologies, E4440A,": Instrument.SpectrumAnalyser.AgilentE4440A,
+        # HP 8594E 9e3-40e9
+        # HP 8653E -26.5e9
+
 
     })
 
@@ -180,26 +167,26 @@ with ResourceManager('') as rm:
     sheetname = input("Sheetname --> ")
     ws.title = sheetname
     ws.append(["Frequency", "Mean dBm", "stddev", "list dBm"])
-    # SpectrumAnalyser[0].setup("Narrow CW Power + 10MHz output enabled")
-    freq = "{0:.0f}".format(float(input("Wanted frequency for peaking GHz: ")) * 1e9)
+    SpectrumAnalyser[0].setup("Narrow CW Power + 10MHz output enabled")
+    freq = "{0:.0f}".format(float(input("Wanted frequency for peaking in GHz: ")) * 1e9)
 
-    # SignalGenerator[0].freq(freq)
-    # SignalGenerator[0].amplimit = 10
-    # SignalGenerator[0].amp(10)
-    # SignalGenerator[0].enable()
-    # SpectrumAnalyser[0].CF(freq)
+    SignalGenerator[0].freq(freq)
+    SignalGenerator[0].amplimit = 10
+    SignalGenerator[0].amp(10)
+    SignalGenerator[0].enable()
+    SpectrumAnalyser[0].CF(freq)
 
     while input("Manual peak hold, y when ready to sweep --> ") is not "y":
         pass
 
-    EstimatedTime = ETC((18e9-1e9)/100e6)
+    EstimatedTime = ETC((18e9-1e9)/100e6)  # CALCulate number of steps in test
 
     try:
-        for freq in np.arange(1e9, 18e9 + 1, 100e6):
-            freq = "{0:.0f}".format(freq)
+        for freq in np.arange(1e9, 18e9 + 1, 100e6):  # arange is upto but not including max value, thus + 1
+            freq = "{0:.0f}".format(freq)  # Needed? or units filter @decorator
             print(freq)
-            # SpectrumAnalyser[0].CF(freq)
-            # SignalGenerator[0].freq(freq)
+            SpectrumAnalyser[0].CF(freq)
+            SignalGenerator[0].freq(freq)
 
             start = timer()
             time.sleep(1)
@@ -212,16 +199,16 @@ with ResourceManager('') as rm:
                 readings = 10
                 while measure is not True:
 
-                    value = float(PowerMeter[0].query(':FETCh:SCALar:POWer:AC?'))
-                    # freq, amplitude = SpectrumAnalyser[0].measure(freq)
-                    # value = -10  # amplitude
+                    # value = float(PowerMeter[0].query(':FETCh:SCALar:POWer:AC?'))
+                    freq, amplitude = SpectrumAnalyser[0].measure(freq)
+                    value = amplitude
                     # print(value)
                     meas.append(value)
                     # print(meas)
                     if len(meas) > readings:
                         meas.pop(0)  # remove item at index 0
                         stddev = statistics.stdev(meas)
-                        # print(stddev)
+                        print(stddev)
                         if stddev < stddevtolerance:
                             measure = True
                     time.sleep(delay)
@@ -233,15 +220,15 @@ with ResourceManager('') as rm:
                 ws.append([freq, statistics.mean(meas), stddev] + meas)
 
                 EstimatedTime.append(timer() - start)  # end - start
-                print("ETC: " + str(EstimatedTime.ETC()) + " s")
+                print("ETC: {} s".format(EstimatedTime.ETC()))
                 print()
 
     finally:
-        # generator[0].disable()
+        generator[0].disable()
         wb.save(filename + ".xlsx")
 
     '''
-    Pause off tes
+    Pause off test
     Display table during test - of last ten points # pandas
     Display graph during test # matplotlib
     '''
