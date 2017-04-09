f0500fbb9f24481f8f5f0d66f60db89cc474bb34
David Lutton <david@dalun.space>
2017-04-09 11:35:11 +0100

    Refactor all SignalGenerators to use @propertys

--- engineering_project/Instrument/SignalGenerator.py
+++ engineering_project/Instrument/SignalGenerator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import time
 import logging
+import pint
 
 from Instrument.GenericInstrument import GenericInstrument as GenericInstrument
 
@@ -14,6 +15,11 @@ class SignalGenerator(GenericInstrument):
         self.amplitude = min(self.amps)
         self.frequency = min(self.freqs)
         self.output = False
+
+    def state(self):
+        print("Amplitude: {}".format(self.amplitude))
+        print("Frequency: {}".format(self.frequency))
+        print("Output: {}".format(self.output))
 
     def start(self, lvl=-50):
         self.amplitude = lvl
@@ -38,8 +44,37 @@ class SignalGenerator(GenericInstrument):
     '''
 
 
-class SCPI(SignalGenerator):
+class amplitudelimiter(object):
 
+    def __init__(self, f, *args, **kwargs):
+        """
+        If there are no decorator arguments, the function
+        to be decorated is passed to the constructor.
+        """
+        # print(f)
+        # print(*args)
+        # print(**kwargs)
+        # print("Inside __init__()")
+        self.f = f
+
+    def __call__(self, f, *args, **kwargs):
+        """
+        The __call__ method is not called until the
+        decorated function is called.
+        """
+        # print(f)
+        print(*args)
+        # print(**kwargs)
+        # print("Inside __call__()")
+        setpoint = float(*args)
+        if setpoint > f.amplimit:
+            f.log.warn("Amplimit ({}) reached with setpoint ({}) on {}".format(f.amplimit, setpoint, f.instrument))
+        else:
+            self.f(f, *args)
+        # print("After self.f(*args)")
+
+
+class SCPI(SignalGenerator):
     def __repr__(self):
         return("{}, {}".format(__class__, self.instrument))
 
@@ -52,9 +87,8 @@ class SCPI(SignalGenerator):
         self.amps = [-140, 17]
         self.freqs = [100e3, 3e9]
         # self.siggen.write("*CLS")  # clear error status
+        # self.frequency = min(self.freqs)
 
-#    def __preset__(self):
-#        self.safe()
     @property
     def frequency(self):
         return(self.query("SOURce:FREQuency:CW?"))
@@ -68,29 +102,71 @@ class SCPI(SignalGenerator):
         return(self.query("SOURce:POWer:LEVel:AMPLitude?"))
 
     @amplitude.setter
+    @amplitudelimiter
     def amplitude(self, amplitude):
         self.write("SOURce:POWer:LEVel:AMPLitude {0:.2f} DBM".format(amplitude))
 
-    '''
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-
-                self.amplitude = amplitude
-            else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
-    '''
-
     @property
     def output(self):
-        self.query("OUTPut:STATe?")
+        if self.query("OUTPut:STATe?") == "1":
+            return(True)
+        else:
+            return(False)
 
     @output.setter
-    def output(self, boolean):
-        self.write("OUTPut:STATe {}".format(boolean))
+    def output(self, boolean=False):
+        self.write("OUTPut:STATe {:d}".format(boolean))
 
 
-'''class HP8664A(SignalGenerator):
+class HP8657A(SignalGenerator):
+    def __repr__(self):
+        return("{}, {}".format(__class__, self.instrument))
+
+    def __init__(self, instrument):
+        super().__init__(instrument)
+        self.log.info('Creating {} for {}'.format(str(__class__.__name__), self.instrument))
+
+        assert self.IDN.startswith('HEWLETT-PACKARD,8657A,')
+        self.amps = [-111, 17]
+        self.freqs = [100e3, 1040e6]
+
+        # self.siggen.write("*CLS")  # clear error status
+
+        self.safe()
+        # self.state()
+
+    @property
+    def frequency(self):
+        return(self.query("FR?"))
+
+    @frequency.setter
+    def frequency(self, frequency):
+        self.write("FR {0:.0f}Hz".format(frequency))
+
+    @property
+    def amplitude(self):
+        return(self.query("AP?"))
+
+    @amplitude.setter
+    @amplitudelimiter
+    def amplitude(self, amplitude):
+        self.write("AP {0:.1f}DM".format(amplitude))
+
+    '''
+    @property
+    def output(self):
+        if self.query("OUTPut:STATe?") == "1":
+            return(True)
+        else:
+            return(False)
+
+    @output.setter
+    def output(self, boolean=False):
+        self.write("OUTPut:STATe {:d}".format(boolean))
+    '''
+
+
+class HP8664A(SignalGenerator):
 
     def __repr__(self):
         return("{}, {}".format(__class__, self.instrument))
@@ -104,31 +180,35 @@ class SCPI(SignalGenerator):
         assert self.IDN.startswith('HEWLETT_PACKARD,8664A,')
         self.amps = [-140, 17]
         self.freqs = [100e3, 3e9]
-        self.frequency = self.query("FREQ:CW?")
-        self.amplitude = self.query("AMPL:OUT:LEV?")
-        # self.siggen.write("*CLS")  # clear error status
 
-    def __preset__(self):
-        self.safe()
+        @property
+        def frequency(self):
+            return(self.query("FREQ:CW?"))
 
-    def freq(self, freq):
-        self.freqsetter("FREQ:CW {0:.0f}Hz".format(freq))
+        @frequency.setter
+        def frequency(self, frequency):
+            self.write("FREQ:CW {0:.0f}Hz".format(frequency))
 
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-                self.write("AMPL:OUT:LEV {0:.1f}DBM".format(amplitude))
-                self.amplitude = amplitude
+        @property
+        def amplitude(self):
+            return(self.query("AMPL:OUT:LEV?"))
 
-                # time.sleep(.3)  # after leveling wait time for settling
+        @amplitude.setter
+        @amplitudelimiter
+        def amplitude(self, amplitude):
+            self.write("AMPL:OUT:LEV {0:.1f}DBM".format(amplitude))
+        '''
+        @property
+        def output(self):
+            if self.query("OUTPut:STATe?") == "1":
+                return(True)
             else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
+                return(False)
 
-    def enable(self):
-        pass  # self.write("OUTP:STAT ON")
-
-    def disable(self):
-        pass  # self.write("OUTP:STAT OFF")
+        @output.setter
+        def output(self, boolean=False):
+            self.write("OUTPut:STATe {:d}".format(boolean))
+        '''
 
 
 class HP8665B(SignalGenerator):
@@ -145,70 +225,35 @@ class HP8665B(SignalGenerator):
         assert self.IDN.startswith('HEWLETT_PACKARD,8665B,')
         self.amps = [-140, 17]
         self.freqs = [100e3, 6e9]
-        self.frequency = self.query("FREQ:CW?")
-        self.amplitude = self.query("AMPL:OUT:LEV?")
-        # self.siggen.write("*CLS")  # clear error status
 
-    def __preset__(self):
-        self.safe()
+        @property
+        def frequency(self):
+            return(self.query("FREQ:CW?"))
 
-    def freq(self, freq):
-        self.freqsetter("FREQ:CW {0:.0f}Hz".format(freq))
+        @frequency.setter
+        def frequency(self, frequency):
+            self.write("FREQ:CW {0:.0f}Hz".format(frequency))
 
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-                self.write("AMPL:OUT:LEV {0:.1f}DBM".format(amplitude))
-                self.amplitude = amplitude
+        @property
+        def amplitude(self):
+            return(self.query("AMPL:OUT:LEV?"))
 
-                # time.sleep(.3)  # after leveling wait time for settling
+        @amplitude.setter
+        @amplitudelimiter
+        def amplitude(self, amplitude):
+            self.write("AMPL:OUT:LEV {0:.1f}DBM".format(amplitude))
+        '''
+        @property
+        def output(self):
+            if self.query("OUTPut:STATe?") == "1":
+                return(True)
             else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
+                return(False)
 
-    def enable(self):
-        pass  # self.write("OUTP:STAT ON")
-
-    def disable(self):
-        pass  # self.write("OUTP:STAT OFF")
-
-
-class HP8657A(SignalGenerator):
-    def __repr__(self):
-        return("{}, {}".format(__class__, self.instrument))
-
-    def __init__(self, instrument):
-        super().__init__(instrument)
-        self.log.info('Creating {} for {}'.format(str(__class__.__name__), self.instrument))
-
-        assert self.IDN.startswith('HEWLETT-PACKARD,8657A,')
-        self.amps = [-111, 17]
-        self.freqs = [100e3, 1040e6]
-        self.frequency = self.query("FR?")
-        self.amplitude = self.query("AP?")
-        # self.siggen.write("*CLS")  # clear error status
-        self.__preset__()
-
-    def __preset__(self):
-        self.safe()
-
-    def freq(self, freq):
-        self.freqsetter("FR {0:.0f}Hz".format(freq))
-
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-                self.write("AP {0:.1f}DM".format(amplitude))
-                self.amplitude = amplitude
-
-                # time.sleep(.3)  # after leveling wait time for settling
-            else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
-
-    def enable(self):
-        pass  # self.write("OUTP:STAT ON")
-
-    def disable(self):
-        pass  # self.write("OUTP:STAT OFF")
+        @output.setter
+        def output(self, boolean=False):
+            self.write("OUTPut:STATe {:d}".format(boolean))
+        '''
 
 
 class AgilentE4422B(SignalGenerator):
@@ -222,37 +267,89 @@ class AgilentE4422B(SignalGenerator):
 
         assert self.IDN.startswith('Agilent Technologies, E4422B')
 
-        self.frequency = self.query("FREQ?")
-        self.amplitude = self.query("POW:AMPL?")
         self.amps = [-100, 16]
         self.freqs = [100e3, 4e9]
-        self.write("*CLS")  # clear error status
-        self.safe()
+        # self.write("*CLS")  # clear error status
+        # self.safe()
 
-    def freq(self, freq):
-        if self.frequency != freq:  # prevent resubmitting request to set the same frequency
-            self.write("FREQ {0:.0f} Hz".format(freq))
-            self.frequency = freq
-            time.sleep(.3)  # after retuneing wait time for settling
+        @property
+        def frequency(self):
+            return(self.query("FREQ?"))
 
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-                self.write("POW:AMPL {0:.1f} dBm".format(amplitude))
-                self.amplitude = amplitude
+        @frequency.setter
+        def frequency(self, frequency):
+            self.write("FREQ {0:.0f} Hz".format(frequency))
 
-                # time.sleep(.3)  # after leveling wait time for settling
+        @property
+        def amplitude(self):
+            return(self.query("POW:AMPL?"))
+
+        @amplitude.setter
+        @amplitudelimiter
+        def amplitude(self, amplitude):
+            self.write("POW:AMPL {0:.2f} dBm".format(amplitude))
+
+        @property
+        def output(self):
+            if self.query("OUTPut:STATe?") == "1":
+                return(True)
             else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
+                return(False)
 
-    def enable(self):
-        self.write("OUTP:STAT ON")
-
-    def disable(self):
-        self.write("OUTP:STAT OFF")
+        @output.setter
+        def output(self, boolean=False):
+            self.write("OUTPut:STATe {:d}".format(boolean))  # OUTP:STAT ON // OFF
 
 
-class AnritsuMG3691B(SignalGenerator):  # ANRITSU,MG3691B,
+class AnritsuMG369nx(SignalGenerator):  # ANRITSU,MG369nx
+    # Need to preset : amp offset, freq offset, used freq, used amp, used mod, used pulse
+
+    def __repr__(self):
+        return("{}, {}".format(__class__, self.instrument))
+
+    def __init__(self, instrument):
+        super().__init__(instrument)
+        # self.log.info('Creating an instance of\t' + str(__class__))
+        self.log.info('Creating {} for {}'.format(str(__class__.__name__), self.instrument))
+
+        # self.amps = [-110, 30]
+        self.freqs = [2e9, 10e9]
+        # self.write("*CLS")  # clear error status
+        # self.safe()
+        self.write('RL1')  # Release to Local
+
+        @property
+        def frequency(self):
+            return(self.query("OF0"))
+
+        @frequency.setter
+        def frequency(self, frequency):
+            self.write("F0{0:.0f} HZ".format(frequency))
+
+        @property
+        def amplitude(self):
+            return(self.query("OL0"))  # OLO
+
+        @amplitude.setter
+        @amplitudelimiter
+        def amplitude(self, amplitude):
+            self.write("L0{0:.2f}DM".format(amplitude))
+
+        @property
+        def output(self):
+            pass
+            ''' ORF if self.query("OUTPut:STATe?") == "1":
+                return(True)
+            else:
+                return(False)
+            '''
+
+        @output.setter
+        def output(self, boolean=False):
+            self.write("RF{:d}".format(boolean))
+
+
+class AnritsuMG3691B(AnritsuMG369nx):  # ANRITSU,MG3691B,
     # Need to preset : amp offset, freq offset, used freq, used amp, used mod, used pulse
 
     def __repr__(self):
@@ -265,88 +362,11 @@ class AnritsuMG3691B(SignalGenerator):  # ANRITSU,MG3691B,
 
         assert self.IDN.startswith('ANRITSU,MG3691B,')
 
-        self.frequency = self.query("OF0")
-        self.amplitude = self.query("OLO")
         self.amps = [-110, 30]
         self.freqs = [10e6, 10e9]
-        self.write("*CLS")  # clear error status
-        self.__preset__()
-
-    def __preset__(self):
-        self.safe()
-        self.write('RL1')  # Release to Local
-
-    def freq(self, freq):
-        if self.frequency != freq:  # prevent resubmitting request to set the same frequency
-            self.write("F0{0:.0f} HZ".format(freq))
-            self.frequency = freq
-            time.sleep(.3)  # after retuneing wait time for settling
-
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-                self.write("L0{0:.1f}DM".format(amplitude))
-                self.amplitude = amplitude
-
-                # time.sleep(.3)  # after leveling wait time for settling
-            else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
-
-    def enable(self):
-        self.write("RF1")
-
-    def disable(self):
-        self.write("RF0")
 
 
-class AnritsuMG3693A(SignalGenerator):  # ANRITSU,MG3693A,
-    # Need to preset : amp offset, freq offset, used freq, used amp, used mod, used pulse
-
-    def __repr__(self):
-        return("{}, {}".format(__class__, self.instrument))
-
-    def __init__(self, instrument):
-        super().__init__(instrument)
-        # self.log.info('Creating an instance of\t' + str(__class__))
-        self.log.info('Creating {} for {}'.format(str(__class__.__name__), self.instrument))
-
-        assert self.IDN.startswith('ANRITSU,MG3693A,')
-
-        self.frequency = self.query("OF0")
-        self.amplitude = self.query("OLO")
-        self.amps = [-110, 30]  # ???
-        self.freqs = [2e9, 30e9]
-        self.write("*CLS")  # clear error status
-        self.__preset__()
-
-    def __preset__(self):
-        self.safe()
-        self.write('RL1')  # Release to Local
-
-    def freq(self, freq):
-        if self.frequency != freq:  # prevent resubmitting request to set the same frequency
-            self.write("F0{0:.0f} HZ".format(freq))
-            self.frequency = freq
-            time.sleep(.3)  # after retuneing wait time for settling
-
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-                self.write("L0{0:.1f}DM".format(amplitude))
-                self.amplitude = amplitude
-
-                # time.sleep(.3)  # after leveling wait time for settling
-            else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
-
-    def enable(self):
-        self.write("RF1")
-
-    def disable(self):
-        self.write("RF0")
-
-
-class AnritsuMG3692A(SignalGenerator):  # ANRITSU,MG3692A,
+class AnritsuMG3692A(AnritsuMG369nx):  # ANRITSU,MG3692A,
     # Need to preset : amp offset, freq offset, used freq, used amp, used mod, used pulse
 
     def __repr__(self):
@@ -359,36 +379,22 @@ class AnritsuMG3692A(SignalGenerator):  # ANRITSU,MG3692A,
 
         assert self.IDN.startswith('ANRITSU,MG3692A,')
 
-        self.frequency = self.query("OF0")
-        self.amplitude = self.query("OLO")
         self.amps = [-110, 30]
         self.freqs = [10e6, 20e9]
-        self.write("*CLS")  # clear error status
-        self.__preset__()
 
-    def __preset__(self):
-        self.safe()
-        self.write('RL1')  # Release to Local
 
-    def freq(self, freq):
-        if self.frequency != freq:  # prevent resubmitting request to set the same frequency
-            self.write("F0{0:.0f} HZ".format(freq))
-            self.frequency = freq
-            time.sleep(.3)  # after retuneing wait time for settling
+class AnritsuMG3693A(AnritsuMG369nx):  # ANRITSU,MG3693A,
+    # Need to preset : amp offset, freq offset, used freq, used amp, used mod, used pulse
 
-    def amp(self, amplitude):
-        if self.amplitude != amplitude:
-            if amplitude <= self.amplimit:  # prevent resubmitting request to set the same frequency
-                self.write("L0{0:.1f}DM".format(amplitude))
-                self.amplitude = amplitude
+    def __repr__(self):
+        return("{}, {}".format(__class__, self.instrument))
 
-                # time.sleep(.3)  # after leveling wait time for settling
-            else:
-                self.log.warn("on " + self.IDN + " exceeded amplimit")
+    def __init__(self, instrument):
+        super().__init__(instrument)
+        # self.log.info('Creating an instance of\t' + str(__class__))
+        self.log.info('Creating {} for {}'.format(str(__class__.__name__), self.instrument))
 
-    def enable(self):
-        self.write("RF1")
+        assert self.IDN.startswith('ANRITSU,MG3693A,')
 
-    def disable(self):
-        self.write("RF0")
-'''
+        self.amps = [-110, 30]
+        self.freqs = [2e9, 30e9]
