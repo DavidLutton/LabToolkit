c5b030af31c4a83e9a6db367ffd260c06ecbdf86
David Lutton <david@dalun.space>
2017-10-01 09:15:22 +0100

    Add AnaheimAutomationSMC40(Positioner)

--- labtoolkit/Positioner.py
+++ labtoolkit/Positioner.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-import time
-import logging
-# from scipy.interpolate import UnivariateSpline
-# import numpy as np
+
+# import pint
 
 try:
     from labtoolkit.GenericInstrument import GenericInstrument
@@ -15,7 +13,13 @@ except ImportError:
     from SCPI import SCPI
 
 
+ureg = pint.UnitRegistry()
+# ureg.default_format = '~P'
+Q_ = ureg.Quantity
+
+
 class Positioner(GenericInstrument):
+
     def __init__(self, instrument):
         super().__init__(instrument)
 
@@ -23,7 +27,61 @@ class Positioner(GenericInstrument):
         return("{}, {}".format(__class__, self.instrument))
 
 
+class AnaheimAutomationSMC40(Positioner):
+
+    # http://www.anaheimautomation.com/manuals/stepper/L010098%20-%20SMC40%20Users%20Guide.pdf
+
+    def __init__(self, instrument):
+        super().__init__(instrument)
+
+    def __repr__(self):
+        return("{}, {}".format(__class__, self.instrument))
+
+    @property
+    def heading(self):
+        return NotImplemented
+
+    @heading.setter
+    def heading(self, clockwise=True):
+        if clockwise is True:
+            self.write('D+')  # Clockwise
+        else:
+            self.write('D-')  # Anti-Clockwise
+
+    @property
+    def stepangle(self):
+        return NotImplemented
+
+    @stepangle.setter
+    def stepangle(self, stepangle=1):  # 100 is 1 degree
+        # self.write('XMN{}'.format(int(stepsize*100)))
+        # int(Q_(stepangle, 'degree').magnitude * 100)
+        self.write('XMN{}'.format(int(Q_(stepangle, 'degree').magnitude * 100)))
+
+    def zero(self):
+        self.write('XGH')
+
+    def init(self):
+        self.write('@0')
+
+    def IDN(self):
+        return self.query('ID')
+
+    def WAI(self):
+        return self.query('XMC')  # ??
+
+    def TRG(self):
+        self.write('XGR')
+
+    def move(self, clockwise=True, angle=1, wait=True):
+        self.stepsize = angle
+        self.heading = clockwise
+        self.write('XGR')
+        if wait is True:
+            self.WAI()
+
+
 REGISTER = {
-    "ZZZ": Positioner,
+    'SMC40': AnaheimAutomationSMC40,
 
 }
