b244d92ff88df22f2f667084f9bd6abae1215967
David Lutton <david@dalun.space>
2017-10-25 19:36:00 +0100

    Move helpers to __init__

--- labtoolkit/__init__.py
+++ labtoolkit/__init__.py
@@ -94,6 +94,82 @@ class Instrument(object):
         self.inst.close()
 
 
+def visaaddresslist(listofaddresses, prefix="GPIB0", suffix="65535::INSTR"):
+    """Generate full address for list of bus address.
+
+    :param listofaddresses: list of integers which are instrument addresses
+    :param prefix: prefix for the bus
+    :param suffix: suffix for the bus
+    :returns: list of fully formed address of instuments
+    """
+    instrs = []
+    for inst in listofaddresses:
+        instrs.append("{}::{}::{}".format(prefix, inst, suffix))
+    return(instrs)
+
+
+#  'TCPIP::192.168.1.113::INSTR'
+
+
+def visaenumerate(rm, list_resources):
+    """Try to discover IDNs for autodiscoverd instruments on a bus."""
+    try:
+        pool = {}
+        # if rm.list_resources() is not None:
+        for resource in list_resources:
+            # print(resource)
+
+            inst = rm.open_resource(
+                resource,
+                read_termination='\n',
+                write_termination='\r\n' if resource.startswith('ASRL') else '\n'
+            )
+
+            try:
+                query = "*IDN?"
+                response = inst.query(query)
+                # log.debug("Query {}:{} {}".format(resource, query, response))
+                IDN = response
+
+            except visa.VisaIOError:  # This was found with print(dir(visa))
+                pass  # traceback.print_exc(file=sys.stdout)
+                IDN = "NONE"
+            finally:
+                pool[inst] = IDN
+
+        return(pool)
+    except ValueError:
+        pass
+    except OSError:
+        pass
+    except FileNotFoundError:
+        print("File for SIM not found")
+
+
+def driverdispatcher(pool, driverlist):
+    """Pair IDN with corresponding driver class."""
+    alloc = {}
+    insts = 0
+    for inst in pool:
+        for driver in driverlist:
+            if pool[inst].startswith(driver):
+                alloc[insts] = driverlist[driver](inst)
+                insts += 1
+    return(alloc)
+
+
+class Instruments(object):
+
+    def __repr__(self):
+        # return str(self.__dict__)
+        buffer = []
+        for key, value in instrument.__dict__.items():
+            buffer.append('{}:'.format(key))
+            for valu in value:
+                buffer.append('     {}: {}'.format(valu, value[valu]))
+        return str('\n'.join(buffer))
+
+
 '''
 print()
 print()
