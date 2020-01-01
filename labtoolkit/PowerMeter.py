23aa25c1fdeda1f91707b943c2341b8366236c50
David Lutton <david@dalun.space>
2020-01-01 11:47:43 +0000

    Crude merge of drivers

--- labtoolkit/PowerMeter.py
+++ labtoolkit/PowerMeter.py
@@ -47,7 +47,12 @@ class AgilentE4418B(PowerMeter, SCPI, IEEE488):
         self.log.info(f'Creating {str(__class__.__name__)} for {self.instrument}')
         # self.log.info('Creating an instance of\t' + str(__class__))
 
-        assert self.IDN.startswith("HEWLETT-PACKARD,E4418B,") or self.IDN.startswith('Agilent Technologies,E4418B,')
+
+        # assert self.IDN.startswith("HEWLETT-PACKARD,E4418B,") or self.IDN.startswith('Agilent Technologies,E4418B,')
+        self.inst = inst
+        self.inst.read_termination = '\n'
+        self.inst.write_termination = '\n'
+
         # self.write('*CLS')
 
         # self.__preset__()
@@ -346,7 +351,7 @@ TR2 trigger with delay
 '''
 
 
-class RohdeSchwarzNRVS(PowerMeter):
+class RohdeSchwarz_NRVS(PowerMeter):
     """Rohde-Schwarz NRVS.
 
     .. figure::  images/PowerMeter/RohdeSchwarzNRVS.jpg
@@ -357,11 +362,17 @@ class RohdeSchwarzNRVS(PowerMeter):
         super().__init__(instrument)
 
         self.log.info(f'Creating {str(__class__.__name__)} for {self.instrument}')
+        self.inst = inst
+        self.inst.read_termination = '\r\n'
+        self.inst.write_termination = ''
+
+        self.inst.write('KF1')
 
     def __repr__(self):
         """."""
         return(f"{__class__}, {self.instrument}")
 
+    '''
     @property
     def measurement(self):
         """Get reading of power level."""
@@ -370,6 +381,27 @@ class RohdeSchwarzNRVS(PowerMeter):
         value = float(vals[-1])
         # print('{} {}'.format(value, unit))
         return(value)
+    '''
+
+    '''
+    inst.write('KF1')  # enable freq correction
+    inst.write('KF0')
+    inst.query('ZV')
+    inst.query('Z2')
+    inst.query('ST')
+    '''
+
+    @property
+    def frequency(self):
+        return NotImplemented
+
+    @frequency.setter
+    def frequency(self, frequency):
+        self.inst.write(f'DF{int(frequency)}')
+
+    @property
+    def power(self):
+        return float(self.inst.query('X3').split(' ')[-1])  # '   DBM   6.94910E+00'
 
 
 class RohdeSchwarzURV4(PowerMeter):
@@ -386,11 +418,90 @@ class Bird4421(PowerMeter):
     """
 
 
+class MI_6960():
+
+    def __init__(self, inst):
+        self.inst = inst
+        self.inst.read_termination = '\r\n'
+        self.inst.write_termination = '\n'
+
+        # self.inst.write('RE')  # Reset
+        # time.sleep(1)
+        # self.calfactor = 100
+        # self.linearityfactor = 10.0
+        # self.offset = 0.0
+        # self.reference = 1
+
+    # UN 0 dBm, 1 Watts
+    # inst.query('?')  # 'VBD-0886E-03'
+    # inst.query('RS').split(',')  # '6960B,UN0,DR-0000E+00,SR2,AV+2000E-02,LF+8000E-03,DC+1000E-01,CF+1000E-01,PR1,TR00,SQ0,RF0,HF0,AA1,PU1,PK0,IS3'
+    # float(inst.query('RS').split(',')[7][2:])  # 100
+    # inst.query('RC')  # 'AZ+2121E+00,AC+3195E+00,OA+1800E-02,OB+4500E-02,OC+2300E-02,OD+2300E-02,OE+2200E-02'
+    # float(inst.query('?')[3:])
+    # inst.read()
+
+    # inst.write('AC')
+    # inst.write('AZ')
+
+    def calibrate(self):
+        self.inst.write('AZ, AC')  # requires lin f and cal f
+
+    @property
+    def settings(self):
+        return self.inst.query('RS').split(',')  # '6960B,UN0,DR-0000E+00,SR2,AV+2000E-02,LF+8000E-03,DC+1000E-01,CF+1000E-01,PR1,TR00,SQ0,RF0,HF0,AA1,PU1,PK0,IS3'
+
+    @property
+    def calfactor(self):
+        return NotImplemented
+
+    @calfactor.setter
+    def calfactor(self, calfactor):
+        self.inst.write(f'CF{calfactor}E')
+
+    @property
+    def offset(self):
+        return NotImplemented
+
+    @offset.setter
+    def offset(self, offset):
+        self.inst.write(f'DR{offset}E')  # 0.0 okay, 0 not accepted
+
+    @property
+    def linearityfactor(self):
+        return NotImplemented
+
+    @linearityfactor.setter
+    def linearityfactor(self, linearityfactor):  # 0.1 to 14.99
+        self.inst.write(f'LF{linearityfactor}E')  # int
+
+    @property
+    def averages(self):
+        return NotImplemented
+
+    @averages.setter
+    def averages(self, averages):
+        self.inst.write(f'AV{averages}E')
+
+    @property
+    def reference(self):  # Power Reference
+        return NotImplemented
+
+    @reference.setter
+    def reference(self, reference):
+        self.inst.write(f'PR{reference}')
+
+    @property
+    def power(self):
+        self.inst.write('TR00')  # soft reset of measurand
+        return float(self.inst.query('?', delay=10)[3:])  # 'VBD-0886E-03'  # ? works but still causes error 07 command error
+
+
 REGISTER = {
     "HEWLETT-PACKARD,437B,": HP437B,
     "Agilent Technologies,E4418B,": AgilentE4418B,
     "HEWLETT-PACKARD,E4418B,": AgilentE4418B,
-    # NVRS
+    # '': RohdeSchwarz_NRVS,  # TODO
+    # '': MI_6960,  # TODO
     # R&S ???4
     # Bird 4421
     # Benchview Supported N1911A, N1912A, N1913A, N1914A, N8262A,
