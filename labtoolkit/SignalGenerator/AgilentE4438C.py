d844fa6488d07108540ddfb5c26a269c3ab7598e
David Lutton <david@dalun.space>
2023-07-24 16:53:44 +0100

    Add arb send & catalog to AgilentE4438C

--- labtoolkit/SignalGenerator/AgilentE4438C.py
+++ labtoolkit/SignalGenerator/AgilentE4438C.py
@@ -15,3 +15,16 @@ class AgilentE4438C(SCPISignalGenerator):
             is_big_endian=True
         )
         return arb.view(np.complex128)
+        
+    def arb_send(self, name, waveform):
+        """±32384."""
+        # iq.view(np.float64).astype(np.int16)
+        return self.write_binary_values(
+            f':MEM:DATA "WFM1:{name}",',
+            waveform,
+            datatype='h', 
+            is_big_endian=True
+        )
+    
+    def arb_catalog(self):
+        return self.query(':MEM:CATalog? "WFM1:"')
