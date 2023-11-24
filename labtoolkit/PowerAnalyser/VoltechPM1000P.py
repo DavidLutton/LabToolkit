1cb8127c3ba7407015a174dfa14bae33ed7ebfd4
David Lutton <David.Lutton@gmail.com>
2023-11-24 21:38:29 +0000

    Sort out PowerAnalyser directory

--- labtoolkit/PowerAnalyser/VoltechPM1000P.py
+++ labtoolkit/PowerAnalyser/VoltechPM1000P.py
@@ -1,38 +1,12 @@
-#!/usr/bin/env python3
-"""."""
-# import time
-# import logging
-# from scipy.interpolate import UnivariateSpline
-# import numpy as np
-from labtoolkit.GenericInstrument import GenericInstrument
-from labtoolkit.IEEE488 import IEEE488
-from labtoolkit.SCPI import SCPI
+from ..IEEE488 import IEEE488
 
 
-class PowerAnalyser(GenericInstrument):
-    """Parent class for PowerAnalysers."""
-
-    def __init__(self, instrument):
-        """."""
-        super().__init__(instrument)
-
-    def __repr__(self):
-        """."""
-        return f"{__class__}, {self.instrument}"
-
-
-class VoltechPM3000A(PowerAnalyser, IEEE488):
-    """Voltech PM3000A.
-
-    .. figure::  images/PowerAnalyser/VoltechPM3000A.jpg
-    """
-
-
-class VoltechPM1000P(PowerAnalyser, IEEE488):
+class VoltechPM1000P(IEEE488):
     """Voltech PM1000+.
 
     .. figure::  images/PowerAnalyser/VoltechPM1000P.jpg
     """
+    pass
 
     '''
     “*CLS” Clears Standard Event Status Enable
@@ -412,11 +386,3 @@ class VoltechPM1000P(PowerAnalyser, IEEE488):
     Switch on equipment under test
     :FRD? Returns Apk+ and Apk-.
     '''
-
-
-REGISTER = {
-    'VoltechPM3000A': VoltechPM3000A,
-    'VoltechPM1000P': VoltechPM1000P,
-    # Benchview suppported  PA2201A, PA2203A
-
-}
---
+++ labtoolkit/PowerAnalyser/VoltechPM3000A.py
@@ -0,0 +1,9 @@
+from ..IEEE488 import IEEE488
+
+
+class VoltechPM3000A(IEEE488):
+    """Voltech PM3000A.
+
+    .. figure::  images/PowerAnalyser/VoltechPM3000A.jpg
+    """
+    pass
