6f2b23f49bfd18072619aa8a526fd629ee12de01
David Lutton <David.Lutton@gmail.com>
2024-03-21 22:19:47 +0000

    Add basic query for HP 3457A

--- src/labtoolkit/DigitalMultimeter/HP3457A.py
+++ src/labtoolkit/DigitalMultimeter/HP3457A.py
@@ -1,4 +1,14 @@
 from ..IEEE488 import IEEE488
 
+
 class HP3457A(IEEE488):
-    ...
+    
+    def __post__(self):
+        self.inst.write_termination = '\n'
+        self.inst.read_termination = '\r\n'
+        # self.write('OFORMAT ASCII')
+    
+    @property
+    def reading(self):
+        return self.query_float('?')
+
